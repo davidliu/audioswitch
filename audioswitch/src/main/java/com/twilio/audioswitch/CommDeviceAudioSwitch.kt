a99af46c53d51ee9be649c1256a29e449b5f98e0
davidliu <davidliu@deviange.net>
2026-04-21 01:59:53 +0900

    Have CommDeviceAudioSwitch match previous earpiece & wired behavior.

--- audioswitch/src/main/java/com/twilio/audioswitch/CommDeviceAudioSwitch.kt
+++ audioswitch/src/main/java/com/twilio/audioswitch/CommDeviceAudioSwitch.kt
@@ -7,6 +7,7 @@ import android.os.Handler
 import android.os.Looper
 import androidx.annotation.RequiresApi
 import androidx.annotation.VisibleForTesting
+import com.twilio.audioswitch.AudioDevice.*
 import com.twilio.audioswitch.android.Logger
 import com.twilio.audioswitch.android.ProductionLogger
 import com.twilio.audioswitch.scanners.CommunicationDeviceScanner
@@ -25,6 +26,22 @@ class CommDeviceAudioSwitch : AbstractAudioSwitch {
     private val communicationDeviceScanner: CommunicationDeviceScanner
         get() = deviceScanner as CommunicationDeviceScanner
 
+    /**
+     * Constructs a new AudioSwitch instance.
+     * - [context] - An Android Context.
+     * - [loggingEnabled] - Toggle whether logging is enabled. This argument is false by default.
+     * - [audioFocusChangeListener] - A listener that is invoked when the system audio focus is updated.
+     * Note that updates are only sent to the listener after [activate] has been called.
+     * - [preferredDeviceList] - The order in which [AudioSwitch] automatically selects and activates
+     * an [AudioDevice]. This parameter is ignored if the [selectedAudioDevice] is not `null`.
+     * The default preferred [AudioDevice] order is the following:
+     * [BluetoothHeadset], [WiredHeadset], [Earpiece], [Speakerphone]
+     * . The [preferredDeviceList] is added to the front of the default list. For example, if [preferredDeviceList]
+     * is [Speakerphone] and [BluetoothHeadset], then the new preferred audio
+     * device list will be:
+     * [Speakerphone], [BluetoothHeadset], [WiredHeadset], [Earpiece].
+     * An [IllegalArgumentException] is thrown if the [preferredDeviceList] contains duplicate [AudioDevice] elements.
+     */
     @JvmOverloads
     constructor(
         context: Context,
@@ -66,25 +83,15 @@ class CommDeviceAudioSwitch : AbstractAudioSwitch {
         }
     }
 
-    /**
-     * Mirrors [CommunicationDeviceScanner] connect events: each [audioDevice] reported by the
-     * scanner is added to [availableUniqueAudioDevices] with no wired/earpiece suppression.
-     */
-    override fun onDeviceConnected(audioDevice: AudioDevice) {
-        this.logger.d(TAG_AUDIO_SWITCH, "onDeviceConnected($audioDevice)")
-        val wasAdded = this.availableUniqueAudioDevices.add(audioDevice)
-        this.selectAudioDevice(wasListChanged = wasAdded)
-    }
-
-    /**
-     * Mirrors [CommunicationDeviceScanner] disconnect events: removes [audioDevice] from
-     * [availableUniqueAudioDevices] and clears a matching user selection.
-     */
     override fun onDeviceDisconnected(audioDevice: AudioDevice) {
         this.logger.d(TAG_AUDIO_SWITCH, "onDeviceDisconnected($audioDevice)")
-        val wasChanged = this.availableUniqueAudioDevices.remove(audioDevice)
+        var wasChanged = this.availableUniqueAudioDevices.remove(audioDevice)
         if (this.userSelectedAudioDevice == audioDevice) {
             this.userSelectedAudioDevice = null
+        }
+
+        if (audioDevice is WiredHeadset && this.audioDeviceManager.hasEarpiece()) {
+            wasChanged = this.availableUniqueAudioDevices.add(Earpiece()) || wasChanged
         }
         this.selectAudioDevice(wasChanged)
     }
